0d3927ba7454bb25c2b4f1131fa1d42d0a656a9d
Preisler25 <preisler.andras@gmail.com>
2024-09-01 22:16:07 +0200

    feat: creation of the emotion controller

--- src/main/java/preisler/com/crazy_counter/controllers/EmotionController.java
+++ src/main/java/preisler/com/crazy_counter/controllers/EmotionController.java
@@ -1,10 +1,15 @@
 package preisler.com.crazy_counter.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.stereotype.Controller;
-
-@Controller
+@RestController
 public class EmotionController {
 
+    @RequestMapping(value="/get", method = RequestMethod.GET)
+    public String index() {
+        return "crazy!!! :D";
+    }
     
 }
