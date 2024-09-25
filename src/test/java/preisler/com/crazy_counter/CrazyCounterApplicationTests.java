8700a30066396f99dc17e6d9a260c08d9958c396
Preisler25 <preisler.andras@gmail.com>
2024-09-25 22:21:14 +0200

    rm: test

--- src/test/java/preisler/com/crazy_counter/CrazyCounterApplicationTests.java
+++ src/test/java/preisler/com/crazy_counter/CrazyCounterApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import preisler.com.crazy_counter.models.Emotion;
+
 import preisler.com.crazy_counter.emotion.EmotionEntity;
 import preisler.com.crazy_counter.emotion.EmotionRepository;
 import preisler.com.crazy_counter.emotion.EmotionService;
