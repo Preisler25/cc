39927cb262e74d1b004ac1a22394989dad069bd8
Preisler25 <preisler.andras@gmail.com>
2024-11-22 13:27:23 +0100

    test: del

--- src/test/java/preisler/com/crazy_counter/CrazyCounterApplicationTests.java
+++ src/test/java/preisler/com/crazy_counter/CrazyCounterApplicationTests.java
@@ -11,21 +11,5 @@ import preisler.com.crazy_counter.emotion.EmotionService;
 
 @SpringBootTest
 class CrazyCounterApplicationTests {
-
-	@Mock
-	private EmotionRepository emotionRepository;
-
-	@InjectMocks
-	private EmotionService emotionService;
-
-	// Initialize mocks
-	public CrazyCounterApplicationTests() {
-		MockitoAnnotations.openMocks(this);
-	}
-
-	@Test
-	void contextLoads() {
-		// Basic test to check if the Spring context loads correctly
-	}
 	
 }
