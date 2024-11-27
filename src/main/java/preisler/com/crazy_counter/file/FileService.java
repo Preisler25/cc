9ba9a135730c62335b1007f198b3e59968897beb
Preisler25 <preisler.andras@gmail.com>
2024-11-27 12:24:44 +0100

    feat: compressing images

--- src/main/java/preisler/com/crazy_counter/file/FileService.java
+++ src/main/java/preisler/com/crazy_counter/file/FileService.java
@@ -1,5 +1,6 @@
 package preisler.com.crazy_counter.file;
 
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
@@ -43,9 +44,43 @@ public class FileService {
         return files;
     }
 
-
-
     public String uploadFile(Long user_id, MultipartFile file) throws IOException {
+        // Create directory if it doesn't exist
+        File directory = new File(uploadDir);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+
+        // Rename the file to user_id_pfp.jpg/png
+        String extension = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf('.'));
+        String filename = user_id + "_pfp" + extension;
+
+        // Prepare the destination file path
+        Path compressedFilePath = Paths.get(uploadDir + filename);
+
+        // Compress the image
+        try {
+            Thumbnails.of(file.getInputStream())
+                    .size(500, 500) // Resize to 500x500 pixels (or any size)
+                    .outputQuality(0.75) // Set quality to 75%
+                    .toFile(compressedFilePath.toFile());
+        } catch (IOException e) {
+            throw new IOException("Error compressing file: " + e.getMessage());
+        }
+
+        System.out.println("Compressed file uploaded: " + filename);
+
+        // Save the file URL to the database
+        userRepository.updateProfilePicture(user_id, filename);
+        System.out.println("File URL saved to database");
+
+        // Return the file URL
+        return "http://192.168.1.199:8080/file/" + filename;
+    }
+
+
+
+    /*public String uploadFile(Long user_id, MultipartFile file) throws IOException {
         // Create directory if it doesn't exist
         File directory = new File(uploadDir);
         if (!directory.exists()) {
@@ -69,6 +104,8 @@ public class FileService {
         // Return the file URL
         return "http://192.168.1.199:8080/file/" + file.getOriginalFilename();
     }
+    */
+
 
     public Resource serveFile(String filename) throws MalformedURLException {
         Path file = Paths.get(uploadDir + filename);
